542eddbdb779d05020fa4708ae6f1f1cb3ea0e81
Rodrigo Silva <rdg.hora1@gmail.com>
2024-09-04 16:51:37 -0300

    Adicionando alguns campos que faltavam para a empresa e salvando os dados no localStorage

--- front-end/src/components/companyForm.ts
+++ front-end/src/components/companyForm.ts
@@ -3,9 +3,14 @@ export function renderCompanyForm(container: HTMLElement | null) {
 
   container.innerHTML = `
         <form id="companyForm">
+
             <div>
                 <label for="companyName">Nome da Empresa:</label>
                 <input type="text" id="companyName" name="companyName">
+            </div>
+              <div>
+                <label for="title">Titulo da vaga:</label>
+                <input type="text" id="title" name="title">
             </div>
             <div>
                 <label for="email">Email:</label>
@@ -14,6 +19,22 @@ export function renderCompanyForm(container: HTMLElement | null) {
             <div>
                 <label for="sector">Setor:</label>
                 <input type="text" id="sector" name="sector">
+            </div>
+              <div>
+                <label for="cnpj">CNPJ:</label>
+                <input type="text" id="cnpj" name="cnpj">
+            </div>
+            <div>
+                <label for="telefone">Telefone:</label>
+                <input type="text" id="telefone" name="telefone">
+            </div>
+            <div>
+                <label for="cep">CEP:</label>
+                <input type="text" id="cep" name="cep">
+            </div>
+            <div>
+                <label for="skills">Location:</label>
+                <input type="text" id="location" name="location">
             </div>
             <div>
                 <label for="description">Descrição:</label>
@@ -27,6 +48,18 @@ export function renderCompanyForm(container: HTMLElement | null) {
   form?.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(form as HTMLFormElement);
-    console.log(Object.fromEntries(formData.entries()));
+    const empresa = Object.fromEntries(formData.entries());
+    const empresas = JSON.parse(localStorage.getItem("empresas") || "[]");
+
+    empresas.push(empresa);
+
+    localStorage.setItem("empresas", JSON.stringify(empresas));
+
+    console.log("Empresa salva:", empresa);
   });
+
+  const verifyTelefone = (telefone: string) => {
+    const telefoneRegex = /^\(?\d{2}\)?\s?9?\d{4}\-?\d{4}$/;
+    return telefoneRegex.test(telefone);
+  };
 }
