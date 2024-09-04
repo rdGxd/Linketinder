434e91dfbbd4099420d179a213db0a9a0931d9dc
Rodrigo Silva <rdg.hora1@gmail.com>
2024-09-04 16:52:15 -0300

    removendo o mock de dentro do candidateList

--- front-end/dist/components/candidateList.js
+++ front-end/dist/components/candidateList.js
@@ -1,25 +1,17 @@
 export function renderCandidateList(container) {
     if (!container)
         return;
-    const candidates = [
-        {
-            skills: ["Python", "JavaScript"],
-            education: "Bacharel em Ciência da Computação",
-        },
-        { skills: ["Java", "C#"], education: "Engenharia de Software" },
-        { skills: ["PHP", "MySQL"], education: "Tecnologia da Informação" },
-    ];
+    const candidates = JSON.parse(localStorage.getItem("candidatos") || "[]");
     const candidateListDiv = document.createElement("div");
     candidateListDiv.innerHTML = `<h3>Candidatos Anônimos</h3>`;
     const ul = document.createElement("ul");
     candidates.forEach((candidate) => {
         const li = document.createElement("li");
         li.innerHTML = `
-            <strong>Competências:</strong> ${candidate.skills.join(", ")}<br>
-            <strong>Formação:</strong> ${candidate.education}
-        `;
+      <strong>Competências:</strong> ${candidate.skills}<br>
+      <strong>Formação:</strong> ${candidate.education}
+    `;
         ul.appendChild(li);
     });
-    candidateListDiv.appendChild(ul);
-    container.appendChild(candidateListDiv);
+    container.appendChild(ul);
 }
--- front-end/src/components/candidateList.ts
+++ front-end/src/components/candidateList.ts
@@ -1,28 +1,19 @@
 export function renderCandidateList(container: HTMLElement | null) {
   if (!container) return;
-
-  const candidates = [
-    {
-      skills: ["Python", "JavaScript"],
-      education: "Bacharel em Ciência da Computação",
-    },
-    { skills: ["Java", "C#"], education: "Engenharia de Software" },
-    { skills: ["PHP", "MySQL"], education: "Tecnologia da Informação" },
-  ];
+  const candidates = JSON.parse(localStorage.getItem("candidatos") || "[]");
 
   const candidateListDiv = document.createElement("div");
   candidateListDiv.innerHTML = `<h3>Candidatos Anônimos</h3>`;
-  const ul = document.createElement("ul");
 
-  candidates.forEach((candidate) => {
+  const ul = document.createElement("ul");
+  candidates.forEach((candidate: any) => {
     const li = document.createElement("li");
     li.innerHTML = `
-            <strong>Competências:</strong> ${candidate.skills.join(", ")}<br>
-            <strong>Formação:</strong> ${candidate.education}
-        `;
+      <strong>Competências:</strong> ${candidate.skills}<br>
+      <strong>Formação:</strong> ${candidate.education}
+    `;
     ul.appendChild(li);
   });
 
-  candidateListDiv.appendChild(ul);
-  container.appendChild(candidateListDiv);
+  container.appendChild(ul);
 }
