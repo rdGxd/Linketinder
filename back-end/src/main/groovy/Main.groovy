58fb446c4f93ad7d0029272e7bc1ea488d65741d
Rodrigo Silva <rdg.hora1@gmail.com>
2024-10-01 09:38:37 -0300

    Removendo o SqlFactory e fazendo alterações no Main.groovy

--- back-end/src/main/groovy/Main.groovy
+++ back-end/src/main/groovy/Main.groovy
@@ -2,13 +2,19 @@ import org.example.controller.CandidatosController
 import org.example.controller.CompetenciasController
 import org.example.controller.EmpresasController
 import org.example.controller.VagaController
+import org.example.model.Candidato
+import org.example.model.Competencia
+import org.example.model.Empresa
+import org.example.model.Vaga
 
 static void main(String[] args) {
     CandidatosController candidatosController = new CandidatosController()
     EmpresasController empresasController = new EmpresasController()
     VagaController vagasController = new VagaController()
     CompetenciasController competenciasController = new CompetenciasController()
-    def sc = new Scanner(System.in)
+    String nome, email, pais, cep, descricao, sobrenome, cpf, senha, dataNascimento, cnpj, local, idAttVaga
+    int id
+    def scanner = new Scanner(System.in)
 
     while (true) {
         println "Escolha uma opção: "
@@ -29,57 +35,173 @@ static void main(String[] args) {
         println "15. Deseja atualizar uma competência ?"
         println "16. Deseja deletar uma competência ?"
         println "0. Sair"
-        int opcao = sc.nextInt()
+        int opcao = Integer.parseInt(scanner.nextLine())
         switch (opcao) {
             case 1:
                 println "Lista de Candidatos:"
-                candidatosController.listar()
+                candidatosController.listar().forEach {
+                    println(it)
+                }
                 break
             case 2:
                 println "Lista de Empresas:"
-                empresasController.listar()
+                empresasController.listar().forEach {
+                    println(it)
+                }
                 break
             case 3:
-                candidatosController.inserir()
+                println "Digite o nome do candidato: "
+                nome = scanner.nextLine()
+                println "Digite o sobrenome do candidato: "
+                sobrenome = scanner.nextLine()
+                println "Digite a data de nascimento do candidato: formato (yyyy-MM-dd)"
+                dataNascimento = scanner.nextLine()
+                println "Digite o email do candidato: "
+                email = scanner.nextLine()
+                println "Digite o cpf do candidato: "
+                cpf = scanner.nextLine()
+                println "Digite o pais do candidato: "
+                pais = scanner.nextLine()
+                println "Digite o cep do candidato: "
+                cep = scanner.nextLine()
+                println "Digite a descrição do candidato: "
+                descricao = scanner.nextLine()
+                println "Digite a senha do candidato: "
+                senha = scanner.nextLine()
+                Candidato newUser = new Candidato(nome, sobrenome, dataNascimento, email, cpf, pais, cep, descricao, senha)
+                candidatosController.inserir(newUser)
                 break
             case 4:
-                empresasController.inserir()
+                println "Digite o nome da empresa: "
+                nome = scanner.nextLine()
+                println "Digite o email da empresa: "
+                email = scanner.nextLine()
+                println "Digite o cnpj da empresa: "
+                cnpj = scanner.nextLine()
+                println "Digite o pais da empresa: "
+                pais = scanner.nextLine()
+                println "Digite o cep da empresa: "
+                cep = scanner.nextLine()
+                println "Digite a descrição da empresa: "
+                descricao = scanner.nextLine()
+                println "Digite a senha da empresa: "
+                senha = scanner.nextLine()
+                Empresa newEmpresa = new Empresa(nome, cnpj, email, descricao, pais, cep, senha)
+                empresasController.inserir(newEmpresa)
                 break
             case 5:
-                candidatosController.atualizar()
+                println("Informe o código do produto: ")
+                id = Integer.parseInt(scanner.nextLine())
+                println "Digite o nome do candidato: "
+                nome = scanner.nextLine()
+                println "Digite o sobrenome do candidato: "
+                sobrenome = scanner.nextLine()
+                println "Digite a data de nascimento do candidato: formato (yyyy-MM-dd)"
+                dataNascimento = scanner.nextLine()
+                println "Digite o email do candidato: "
+                email = scanner.nextLine()
+                println "Digite o cpf do candidato: "
+                cpf = scanner.nextLine()
+                println "Digite o pais do candidato: "
+                pais = scanner.nextLine()
+                println "Digite o cep do candidato: "
+                cep = scanner.nextLine()
+                println "Digite a descrição do candidato: "
+                descricao = scanner.nextLine()
+                println "Digite a senha do candidato: "
+                senha = scanner.nextLine()
+                Candidato newUser = new Candidato(nome, sobrenome, dataNascimento, email, cpf, pais, cep, descricao, senha)
+                candidatosController.atualizar(newUser, id)
                 break
             case 6:
-                candidatosController.deletar()
+                println("Informe o código do candidato que deseja deletar: ")
+                id = Integer.parseInt(scanner.nextLine())
+                candidatosController.deletar(id)
                 break
             case 7:
-                empresasController.atualizar()
+                println("Informe o código da empresa que deseja atualizar: ")
+                id = Integer.parseInt(scanner.nextLine())
+                println "Digite o nome da empresa: "
+                nome = scanner.nextLine()
+                println "Digite o email da empresa: "
+                email = scanner.nextLine()
+                println "Digite o cnpj da empresa: "
+                cnpj = scanner.nextLine()
+                println "Digite o pais da empresa: "
+                pais = scanner.nextLine()
+                println "Digite o cep da empresa: "
+                cep = scanner.nextLine()
+                println "Digite a descrição da empresa: "
+                descricao = scanner.nextLine()
+                println "Digite a senha da empresa: "
+                senha = scanner.nextLine()
+                Empresa newEmpresa = new Empresa(nome, cnpj, email, descricao, pais, cep, senha)
+                empresasController.atualizar(newEmpresa, id)
                 break
             case 8:
-                empresasController.deletar()
+                println("Informe o código do produto: ")
+                id = Integer.parseInt(scanner.nextLine())
+                empresasController.deletar(id)
                 break
             case 9:
-                vagasController.listar()
+                vagasController.listar().forEach {
+                    println(it)
+                }
                 break
             case 10:
-                vagasController.inserir()
+                println("Digite o ID da empresa: ")
+                id = Integer.parseInt(scanner.nextLine())
+                println "Digite o nome da vaga: "
+                nome = scanner.nextLine()
+                println "Digite a descrição da vaga: "
+                descricao = scanner.nextLine()
+                println "Digite o local da vaga: "
+                local = scanner.nextLine()
+                Vaga newVaga = new Vaga(id.toString(), nome, descricao, local)
+                vagasController.inserir(newVaga)
                 break
             case 11:
-                vagasController.atualizar()
+                println("Informe o código da vaga que deseja atualizar: ")
+                id = Integer.parseInt(scanner.nextLine())
+                println("Digite o ID da empresa: ")
+                idAttVaga = scanner.nextLine()
+                println "Digite o nome da vaga: "
+                nome = scanner.nextLine()
+                println "Digite a descrição da vaga: "
+                descricao = scanner.nextLine()
+                println "Digite o local da vaga: "
+                local = scanner.nextLine()
+                Vaga newVaga = new Vaga(id.toString(), nome, descricao, local)
+                vagasController.atualizar(newVaga, idAttVaga.toInteger())
                 break
             case 12:
-                vagasController.deletar()
+                println("Informe o código da vaga: ")
+                id = Integer.parseInt(scanner.nextLine())
+                vagasController.deletar(id)
                 break
             case 13:
-                competenciasController.listar()
+                competenciasController.listar().forEach {
+                    println(it)
+                }
                 break
             case 14:
-                competenciasController.inserir()
+                println "Digite o nome da vaga: "
+                nome = scanner.nextLine()
+                Competencia newVaga = new Competencia(nome)
+                competenciasController.inserir(newVaga)
                 break
             case 15:
-                competenciasController.atualizar()
+                println("Informe o código da vaga que deseja atualizar: ")
+                id = Integer.parseInt(scanner.nextLine())
+                println "Digite o nome da vaga: "
+                nome = scanner.nextLine()
+                Competencia newVaga = new Competencia(nome)
+                competenciasController.atualizar(newVaga, id)
                 break
             case 16:
-                competenciasController.deletar()
+                println("Informe o código da vaga: ")
+                id = Integer.parseInt(scanner.nextLine())
+                competenciasController.deletar(id)
                 break
             case 0:
                 println "Saindo..."
