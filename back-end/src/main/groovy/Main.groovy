a0d005e49504d52c9ee99aaef827c2955b93292d
Rodrigo Silva <rdg.hora1@gmail.com>
2024-10-04 11:16:10 -0300

    Ajustando o MAIN

--- back-end/src/main/groovy/Main.groovy
+++ back-end/src/main/groovy/Main.groovy
@@ -2,40 +2,40 @@ import org.example.controller.CandidatosController
 import org.example.controller.CompetenciasController
 import org.example.controller.EmpresasController
 import org.example.controller.VagaController
+import org.example.dao.CandidatoDAO
+import org.example.dao.CompetenciaDAO
+import org.example.dao.EmpresaDAO
+import org.example.dao.VagaDAO
 import org.example.model.Candidato
 import org.example.model.Competencia
 import org.example.model.Empresa
 import org.example.model.Vaga
+import org.example.repository.Postgres
+
+import java.sql.Connection
 
 static void main(String[] args) {
-    CandidatosController candidatosController = new CandidatosController()
-    EmpresasController empresasController = new EmpresasController()
-    VagaController vagasController = new VagaController()
-    CompetenciasController competenciasController = new CompetenciasController()
+    Connection connection = new Postgres().conectar()
+
+    CandidatoDAO candidatoDAO = new CandidatoDAO(connection)
+    EmpresaDAO empresaDAO = new EmpresaDAO(connection)
+    VagaDAO vagaDAO = new VagaDAO(connection)
+    CompetenciaDAO competenciaDAO = new CompetenciaDAO(connection)
+
+    CandidatosController candidatosController = new CandidatosController(candidatoDAO)
+    EmpresasController empresasController = new EmpresasController(empresaDAO)
+    VagaController vagasController = new VagaController(vagaDAO)
+    CompetenciasController competenciasController = new CompetenciasController(competenciaDAO)
+
+    UserInputHandler userInputHandler = new UserInputHandler()
+
     String nome, email, pais, cep, descricao, sobrenome, cpf, senha, dataNascimento, cnpj, local, idAttVaga
     int id
+
     def scanner = new Scanner(System.in)
 
     while (true) {
-        println "Escolha uma opção: "
-        println "1. Listar Candidatos"
-        println "2. Listar Empresas"
-        println "3. Deseja inserir mais candidatos ?"
-        println "4. Deseja inserir mais empresas ?"
-        println "5. Editar Candidato"
-        println "6. Deletar Candidato"
-        println "7. Editar Empresa"
-        println "8. Deletar Empresa"
-        println "9. Listar Vagas"
-        println "10. Deseja inserir mais vagas ?"
-        println "11. Deseja atualizar uma vaga ?"
-        println "12. Deseja deletar uma vaga ?"
-        println "13. Listar Competências"
-        println "14. Deseja inserir mais competências ?"
-        println "15. Deseja atualizar uma competência ?"
-        println "16. Deseja deletar uma competência ?"
-        println "0. Sair"
-        int opcao = Integer.parseInt(scanner.nextLine())
+        int opcao = userInputHandler.getOption()
         switch (opcao) {
             case 1:
                 println "Lista de Candidatos:"
@@ -210,4 +210,5 @@ static void main(String[] args) {
                 println "Opção inválida. Tente novamente."
         }
     }
+
 }
