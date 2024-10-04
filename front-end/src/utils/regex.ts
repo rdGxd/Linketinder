ae704f9d4f0f98836333bcf402dfb4b0fbdf72fe
Rodrigo Silva <rdg.hora1@gmail.com>
2024-10-04 08:13:59 -0300

    Refatoração de regex

--- front-end/src/utils/regex.ts
+++ front-end/src/utils/regex.ts
@@ -1,8 +1,8 @@
-export const telefone = /^\(?\d{2}\)?\s?9?\d{4}\-?\d{4}$/;
-export const cnpj = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/;
-export const cpf = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+export const telefone = /^\(?\d{2}\)?\s?9?\d{4}-?\d{4}$/;
+export const cnpj = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/;
+export const cpf = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
 export const email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/;
-export const cep = /^\d{5}\-\d{3}$/;
+export const cep = /^\d{5}-\d{3}$/;
 export const nome = /^[a-zA-Z]+$/;
 export const linkedin = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9-]+\/?$/;
 
