63ac68e2fb25c39c6adf8f002b4516c5e02e992d
Rodrigo Silva <rdg.hora1@gmail.com>
2024-10-07 12:28:57 -0300

    Refatorando o modo que o inputs sao obtidos

--- back-end/src/main/groovy/Main.groovy
+++ back-end/src/main/groovy/Main.groovy
@@ -11,11 +11,14 @@ import org.example.model.Candidato
 import org.example.model.Competencia
 import org.example.model.Empresa
 import org.example.model.Vaga
-import org.example.utils.UserInputHandler
+import org.example.utils.InputHandler
+import org.example.utils.ShowOptions
 
 import java.sql.Connection
 
 static void main(String[] args) {
+    InputHandler inputHandler = new InputHandler()
+
     Connection connection = new Postgres().conectar()
 
     CandidatoDAO candidatoDAO = new CandidatoDAO(connection)
@@ -28,12 +31,10 @@ static void main(String[] args) {
     VagaController vagasController = new VagaController(vagaDAO)
     CompetenciasController competenciasController = new CompetenciasController(competenciaDAO)
 
-    UserInputHandler userInputHandler = new UserInputHandler()
+    ShowOptions userInputHandler = new ShowOptions()
 
     String nome, email, pais, cep, descricao, sobrenome, cpf, senha, dataNascimento, cnpj, local, idAttVaga
     int id
-
-    def scanner = new Scanner(System.in)
 
     while (true) {
         int opcao = userInputHandler.getOption()
@@ -50,98 +51,62 @@ static void main(String[] args) {
                     println(it)
                 }
                 break
-            case 3:
-                println "Digite o nome do candidato: "
-                nome = scanner.nextLine()
-                println "Digite o sobrenome do candidato: "
-                sobrenome = scanner.nextLine()
-                println "Digite a data de nascimento do candidato: formato (yyyy-MM-dd)"
-                dataNascimento = scanner.nextLine()
-                println "Digite o email do candidato: "
-                email = scanner.nextLine()
-                println "Digite o cpf do candidato: "
-                cpf = scanner.nextLine()
-                println "Digite o pais do candidato: "
-                pais = scanner.nextLine()
-                println "Digite o cep do candidato: "
-                cep = scanner.nextLine()
-                println "Digite a descrição do candidato: "
-                descricao = scanner.nextLine()
-                println "Digite a senha do candidato: "
-                senha = scanner.nextLine()
+            case 3: 1
+                nome = inputHandler.getInput("Digite o nome do candidato: ")
+                sobrenome = inputHandler.getInput("Digite o sobrenome do candidato: ")
+                dataNascimento = inputHandler.getInput("Digite a data de nascimento do candidato: formato (yyyy-MM-dd)")
+                email = inputHandler.getInput("Digite o email do candidato: ")
+                cpf = inputHandler.getInput("Digite o cpf do candidato: ")
+                pais = inputHandler.getInput("Digite o pais do candidato: ")
+                cep = inputHandler.getInput("Digite o cep do candidato: ")
+                descricao = inputHandler.getInput("Digite a descrição do candidato: ")
+                senha = inputHandler.getInput("Digite a senha do candidato: ")
                 Candidato newUser = new Candidato(nome, sobrenome, dataNascimento, email, cpf, pais, cep, descricao, senha)
                 candidatosController.inserir(newUser)
                 break
             case 4:
-                println "Digite o nome da empresa: "
-                nome = scanner.nextLine()
-                println "Digite o email da empresa: "
-                email = scanner.nextLine()
-                println "Digite o cnpj da empresa: "
-                cnpj = scanner.nextLine()
-                println "Digite o pais da empresa: "
-                pais = scanner.nextLine()
-                println "Digite o cep da empresa: "
-                cep = scanner.nextLine()
-                println "Digite a descrição da empresa: "
-                descricao = scanner.nextLine()
-                println "Digite a senha da empresa: "
-                senha = scanner.nextLine()
+                nome = inputHandler.getInput("Digite o nome da empresa: ")
+                email = inputHandler.getInput("Digite o email da empresa: ")
+                cnpj = inputHandler.getInput("Digite o cnpj da empresa: ")
+                pais = inputHandler.getInput("Digite o pais da empresa: ")
+                cep = inputHandler.getInput("Digite o cep da empresa: ")
+                descricao = inputHandler.getInput("Digite a descrição da empresa: ")
+                senha = inputHandler.getInput("Digite a senha da empresa: ")
                 Empresa newEmpresa = new Empresa(nome, cnpj, email, descricao, pais, cep, senha)
                 empresasController.inserir(newEmpresa)
                 break
             case 5:
-                println("Informe o código do produto: ")
-                id = Integer.parseInt(scanner.nextLine())
-                println "Digite o nome do candidato: "
-                nome = scanner.nextLine()
-                println "Digite o sobrenome do candidato: "
-                sobrenome = scanner.nextLine()
-                println "Digite a data de nascimento do candidato: formato (yyyy-MM-dd)"
-                dataNascimento = scanner.nextLine()
-                println "Digite o email do candidato: "
-                email = scanner.nextLine()
-                println "Digite o cpf do candidato: "
-                cpf = scanner.nextLine()
-                println "Digite o pais do candidato: "
-                pais = scanner.nextLine()
-                println "Digite o cep do candidato: "
-                cep = scanner.nextLine()
-                println "Digite a descrição do candidato: "
-                descricao = scanner.nextLine()
-                println "Digite a senha do candidato: "
-                senha = scanner.nextLine()
+                id = inputHandler.getInput("Informe o código do candidato que deseja atualizar: ").toInteger()
+                nome = inputHandler.getInput("Digite o nome do candidato: ")
+                sobrenome = inputHandler.getInput("Digite o sobrenome do candidato: ")
+                dataNascimento = inputHandler.getInput("Digite a data de nascimento do candidato: formato (yyyy-MM-dd)")
+                email = inputHandler.getInput("Digite o email do candidato: ")
+                cpf = inputHandler.getInput("Digite o cpf do candidato: ")
+                pais = inputHandler.getInput("Digite o pais do candidato: ")
+                cep = inputHandler.getInput("Digite o cep do candidato: ")
+                descricao = inputHandler.getInput("Digite a descrição do candidato: ")
+                senha = inputHandler.getInput("Digite a senha do candidato: ")
                 Candidato newUser = new Candidato(nome, sobrenome, dataNascimento, email, cpf, pais, cep, descricao, senha)
                 candidatosController.atualizar(newUser, id)
                 break
             case 6:
-                println("Informe o código do candidato que deseja deletar: ")
-                id = Integer.parseInt(scanner.nextLine())
+                id = inputHandler.getInput("Informe o código do candidato que deseja deletar: ").toInteger()
                 candidatosController.deletar(id)
                 break
             case 7:
-                println("Informe o código da empresa que deseja atualizar: ")
-                id = Integer.parseInt(scanner.nextLine())
-                println "Digite o nome da empresa: "
-                nome = scanner.nextLine()
-                println "Digite o email da empresa: "
-                email = scanner.nextLine()
-                println "Digite o cnpj da empresa: "
-                cnpj = scanner.nextLine()
-                println "Digite o pais da empresa: "
-                pais = scanner.nextLine()
-                println "Digite o cep da empresa: "
-                cep = scanner.nextLine()
-                println "Digite a descrição da empresa: "
-                descricao = scanner.nextLine()
-                println "Digite a senha da empresa: "
-                senha = scanner.nextLine()
+                id = inputHandler.getInput("Informe o código da empresa que deseja atualizar: ").toInteger()
+                nome = inputHandler.getInput("Digite o nome da empresa: ")
+                email = inputHandler.getInput("Digite o email da empresa: ")
+                cnpj = inputHandler.getInput("Digite o cnpj da empresa: ")
+                pais = inputHandler.getInput("Digite o pais da empresa: ")
+                cep = inputHandler.getInput("Digite o cep da empresa: ")
+                descricao = inputHandler.getInput("Digite a descrição da empresa: ")
+                senha = inputHandler.getInput("Digite a senha da empresa: ")
                 Empresa newEmpresa = new Empresa(nome, cnpj, email, descricao, pais, cep, senha)
                 empresasController.atualizar(newEmpresa, id)
                 break
             case 8:
-                println("Informe o código do produto: ")
-                id = Integer.parseInt(scanner.nextLine())
+                id = inputHandler.getInput("Informe o código da empresa que deseja deletar: ").toInteger()
                 empresasController.deletar(id)
                 break
             case 9:
@@ -150,34 +115,24 @@ static void main(String[] args) {
                 }
                 break
             case 10:
-                println("Digite o ID da empresa: ")
-                id = Integer.parseInt(scanner.nextLine())
-                println "Digite o nome da vaga: "
-                nome = scanner.nextLine()
-                println "Digite a descrição da vaga: "
-                descricao = scanner.nextLine()
-                println "Digite o local da vaga: "
-                local = scanner.nextLine()
-                Vaga newVaga = new Vaga(id.toString(), nome, descricao, local)
+                idAttVaga = inputHandler.getInput("Informe o código da empresa: ")
+                nome = inputHandler.getInput("Digite o nome da vaga: ")
+                descricao = inputHandler.getInput("Digite a descrição da vaga: ")
+                local = inputHandler.getInput("Digite o local da vaga: ")
+                Vaga newVaga = new Vaga(idAttVaga, nome, descricao, local)
                 vagasController.inserir(newVaga)
                 break
             case 11:
-                println("Informe o código da vaga que deseja atualizar: ")
-                id = Integer.parseInt(scanner.nextLine())
-                println("Digite o ID da empresa: ")
-                idAttVaga = scanner.nextLine()
-                println "Digite o nome da vaga: "
-                nome = scanner.nextLine()
-                println "Digite a descrição da vaga: "
-                descricao = scanner.nextLine()
-                println "Digite o local da vaga: "
-                local = scanner.nextLine()
+                id = inputHandler.getInput("Informe o código da vaga que deseja atualizar: ").toInteger()
+                idAttVaga = inputHandler.getInput("Informe o código da empresa: ")
+                nome = inputHandler.getInput("Digite o nome da vaga: ")
+                descricao = inputHandler.getInput("Digite a descrição da vaga: ")
+                local = inputHandler.getInput("Digite o local da vaga: ")
                 Vaga newVaga = new Vaga(id.toString(), nome, descricao, local)
                 vagasController.atualizar(newVaga, idAttVaga.toInteger())
                 break
             case 12:
-                println("Informe o código da vaga: ")
-                id = Integer.parseInt(scanner.nextLine())
+                id = inputHandler.getInput("Informe o código da vaga que deseja deletar: ").toInteger()
                 vagasController.deletar(id)
                 break
             case 13:
@@ -186,22 +141,18 @@ static void main(String[] args) {
                 }
                 break
             case 14:
-                println "Digite o nome da vaga: "
-                nome = scanner.nextLine()
+                nome = inputHandler.getInput("Digite o nome da competencia: ")
                 Competencia newVaga = new Competencia(nome)
                 competenciasController.inserir(newVaga)
                 break
             case 15:
-                println("Informe o código da vaga que deseja atualizar: ")
-                id = Integer.parseInt(scanner.nextLine())
-                println "Digite o nome da vaga: "
-                nome = scanner.nextLine()
+                id = inputHandler.getInput("Informe o código da competencia que deseja atualizar: ").toInteger()
+                nome = inputHandler.getInput("Digite o nome da competencia: ")
                 Competencia newVaga = new Competencia(nome)
                 competenciasController.atualizar(newVaga, id)
                 break
             case 16:
-                println("Informe o código da vaga: ")
-                id = Integer.parseInt(scanner.nextLine())
+                id = inputHandler.getInput("Informe o código da competencia que deseja deletar: ").toInteger()
                 competenciasController.deletar(id)
                 break
             case 0:
