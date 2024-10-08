5c417c3ac84c6eb3c3a9fe7bc088b7195b5f375a
Rodrigo Silva <rdg.hora1@gmail.com>
2024-10-08 07:21:27 -0300

    Refatoração do OptionInputHandler

--- back-end/src/main/groovy/org/example/utils/OptionInputHandler.groovy
+++ back-end/src/main/groovy/org/example/utils/OptionInputHandler.groovy
@@ -1,28 +1,41 @@
 package org.example.utils
 
+import org.example.view.ViewOptions
+
 class OptionInputHandler {
     private final Scanner scanner = new Scanner(System.in)
 
-    int getOption() {
+    void getOption() {
+        println()
         println "Escolha uma opção: "
-        println "1. Listar Candidatos"
-        println "2. Listar Empresas"
-        println "3. Deseja inserir mais candidatos ?"
-        println "4. Deseja inserir mais empresas ?"
-        println "5. Editar Candidato"
-        println "6. Deletar Candidato"
-        println "7. Editar Empresa"
-        println "8. Deletar Empresa"
-        println "9. Listar Vagas"
-        println "10. Deseja inserir mais vagas ?"
-        println "11. Deseja atualizar uma vaga ?"
-        println "12. Deseja deletar uma vaga ?"
-        println "13. Listar Competências"
-        println "14. Deseja inserir mais competências ?"
-        println "15. Deseja atualizar uma competência ?"
-        println "16. Deseja deletar uma competência ?"
-        println "0. Sair"
-        return Integer.parseInt(scanner.nextLine())
+        println "1 - Candidatos"
+        println "2 - Empresas"
+        println "3 - Vagas"
+        println "4 - Competências"
+        println "0 - Sair"
+        def option = Integer.parseInt(scanner.nextLine())
+
+        switch (option) {
+            case 1:
+                new ViewOptions().showCandidatoOptions()
+                break
+            case 2:
+                new ViewOptions().showEmpresaOptions()
+                break
+            case 3:
+                new ViewOptions().showVagaOptions()
+                break
+            case 4:
+                new ViewOptions().showCompetenciaOptions()
+                break
+            case 0:
+                break
+            default:
+                println "Opção inválida"
+                getOption()
+        }
+
+
     }
 
 }
