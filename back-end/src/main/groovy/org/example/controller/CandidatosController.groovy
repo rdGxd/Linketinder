2efdac5aea36ba86e3aa700ecdb164b1f45884b0
rdGxd <rdg.hora1@gmail.com>
2024-10-24 09:52:44 -0300

    refactor: Refatorando candidatosController

--- back-end/src/main/groovy/org/example/controller/CandidatosController.groovy
+++ back-end/src/main/groovy/org/example/controller/CandidatosController.groovy
@@ -1,50 +1,36 @@
 package org.example.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
-
-import org.example.dao.CandidatoDAO
+import org.example.service.CandidatoService
 import org.example.model.Candidato
-import org.example.utils.ConnectionFactory
 
 import javax.servlet.ServletException
 import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import java.sql.Connection
+import java.io.IOException
 
 @WebServlet(name = "CandidatosController", urlPatterns = ["/candidatos/*"])
-class CandidatosController  extends HttpServlet {
+class CandidatosController extends HttpServlet {
 
-    private final CandidatoDAO candidatoDAO
+    private final CandidatoService candidatoService
     private final ObjectMapper objectMapper
-    private final Connection connection = ConnectionFactory.getConnectionInstancePostgresHttp()
 
     CandidatosController() {
         this.objectMapper = new ObjectMapper()
-        this.candidatoDAO = new CandidatoDAO(connection)
+        this.candidatoService = new CandidatoService()
     }
 
-    CandidatosController(CandidatoDAO candidatoDAO) {
-        this.candidatoDAO = candidatoDAO
+    CandidatosController(CandidatoService candidatoService) {
+        this.candidatoService = candidatoService
         this.objectMapper = new ObjectMapper()
     }
 
-    List<Candidato> listar() {
-        return candidatoDAO.listar()
-    }
-
-        void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Candidato candidato = objectMapper.readValue(req.getReader(), Candidato.class)
-        candidatoDAO.inserir(candidato)
+        candidatoService.inserir(candidato)
         resp.setStatus(HttpServletResponse.SC_CREATED)
     }
-
-    void inserir(Candidato newUser) {
-        candidatoDAO.inserir(newUser)
-    }
-
-    void atualizar(Candidato newUser, int id) { candidatoDAO.atualizar(newUser, id) }
-
-    void deletar(int id) { candidatoDAO.deletar(id) }
 }
---
+++ back-end/src/main/groovy/org/example/service/CandidatoService.groovy
@@ -0,0 +1,34 @@
+package org.example.service
+
+import org.example.dao.CandidatoDAO
+import org.example.model.Candidato
+import org.example.utils.ConnectionFactory
+
+class CandidatoService {
+
+    private final CandidatoDAO candidatoDAO
+
+    CandidatoService() {
+        this.candidatoDAO = new CandidatoDAO(ConnectionFactory.getConnectionInstancePostgresHttp())
+    }
+
+    CandidatoService(CandidatoDAO candidatoDAO) {
+        this.candidatoDAO = candidatoDAO
+    }
+
+    List<Candidato> listar() {
+        return candidatoDAO.listar()
+    }
+
+    void inserir(Candidato newUser) {
+        candidatoDAO.inserir(newUser)
+    }
+
+    void atualizar(Candidato newUser, int id) {
+        candidatoDAO.atualizar(newUser, id)
+    }
+
+    void deletar(int id) {
+        candidatoDAO.deletar(id)
+    }
+}
